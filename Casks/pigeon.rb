d9953ac8f6483b9b8f78d374ac9bb201187f5a6f
Arnab Bir <arnabbir@gmail.com>
2026-02-20 20:22:33 +0530

    Update pigeon.rb

--- Casks/pigeon.rb
+++ Casks/pigeon.rb
@@ -3,9 +3,10 @@ cask 'pigeon' do
   sha256 '008e8d734e1762e7844f2c5ac653165d42c44bad0b6731e291577e8b2c870ad0'
   
   url "https://github.com/ArnabBir/pigeon-extension/releases/download/v#{version}/Pigeon-#{version}.dmg"
+  url "https://github.com/ArnabBir/pigeon-releases/releases/download/v#{version}/Pigeon-#{version}.dmg"
   name 'Pigeon'
-  desc 'Multi-protocol API testing tool'
-  homepage 'https://github.com/ArnabBir/pigeon-extension'
+  desc 'Pigeon | The Universal Client for Every Network Protocol'
+  homepage 'https://github.com/ArnabBir/pigeon-releases'
   
   app 'Pigeon.app'
   
