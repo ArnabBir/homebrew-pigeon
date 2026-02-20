606872f43150d6c26f95ef21e3288a055820d98f
Arnab Bir <arnabbir@gmail.com>
2026-02-21 02:00:54 +0530

    Update pigeon.rb

--- Casks/pigeon.rb
+++ Casks/pigeon.rb
@@ -1,5 +1,5 @@
 cask 'pigeon' do
-  version '1.0.0'
+  version '1.0.1'
   sha256 '008e8d734e1762e7844f2c5ac653165d42c44bad0b6731e291577e8b2c870ad0'
   
   url "https://github.com/ArnabBir/pigeon-releases/releases/download/v#{version}/Pigeon-#{version}.dmg"
